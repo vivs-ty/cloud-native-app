7b77917711b2086b594ee0bf7f6a769eab5b9537
vivs-ty <vivesht07@gmail.com>
2026-03-27 19:00:11 +0530

    updated localhost

--- main.py
+++ main.py
@@ -66,4 +66,4 @@ async def delete_task(task_id: str):
     raise HTTPException(status_code=404, detail="Task not found")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
